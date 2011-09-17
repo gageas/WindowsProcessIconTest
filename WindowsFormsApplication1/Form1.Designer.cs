bb70d7129f3b5e976a07f6e38f880b15f89b152b
@gageas <arguments.callee@gmail.com>
2011-09-17 23:45:50 +0900

    initial commit

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "アイコン書き換えスレッド起動";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(11, 72);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 50);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "SendMessage(WM_SETICON)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(235, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(200, 50);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "アイコン書き換えスレッド停止";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(234, 72);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(200, 50);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Form#Icon set";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 138);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(629, 291);
+            this.textBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(654, 441);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private const UInt32 WM_GETICON = 0x007f;
+        private const UInt32 WM_SETICON = 0x0080;
+        private IntPtr hIconReplace;
+        private Thread th;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == WM_GETICON)
+            {
+                if ((uint)m.WParam == 1)
+                {
+                    var hIconOld = hIconReplace;
+
+                    // 適当な画像を生成
+                    Bitmap bmp = new Bitmap(32, 32);
+                    using (var g = Graphics.FromImage(bmp))
+                    {
+                        var rand = new Random();
+                        g.DrawRectangle(Pens.Black, new Rectangle(5, 5, 20, 20));
+                        for (var i = 0; i < 10; i++)
+                        {
+                            g.DrawLine(Pens.Red, rand.Next(32), rand.Next(32), rand.Next(32), rand.Next(32));
+                        }
+                    }
+
+                    // アイコン化
+                    hIconReplace = (bmp).GetHicon();
+                    m.Result = hIconReplace;
+
+                    // 旧アイコンを解放(ここでするのは早いかもしんない)
+                    DestroyIcon(hIconOld);
+
+                    var now = System.DateTime.Now;
+                    this.Invoke((MethodInvoker)(
+                        ()=>this.textBox1.AppendText("WM_GETICON(wParam==1)きた " + now.ToLocalTime()+"."+now.Millisecond.ToString("000") + "\n")
+                        ));
+                    return;
+                }
+            }
+            base.WndProc(ref m);
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (th != null) return;
+            th = new Thread(() =>
+            {
+                var rand = new Random();
+                while (true)
+                {
+                    try
+                    {
+                        this.Invoke((MethodInvoker)(() =>
+                        {
+                            var res = SendMessage(this.Handle, WM_SETICON, (IntPtr)0, (IntPtr)(rand.Next()));
+                        }));
+                    }
+                    catch { }
+                    Thread.Sleep(1);
+                }
+            });
+            th.IsBackground = true;
+            th.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SetIcon((IntPtr)(new Random()).Next());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (th != null)
+            {
+                th.Abort();
+                th = null;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Icon = Icon.FromHandle(hIconReplace);
+        }
+
+        private void SetIcon(IntPtr hIcon)
+        {
+            var res = SendMessage(this.Handle, WM_SETICON, (IntPtr)0, hIcon);
+            var err = GetLastError();
+//            MessageBox.Show(res.ToString() + err.ToString());
+        }
+
+        [DllImport("user32.dll")]
+        static extern bool DestroyIcon(IntPtr hIcon);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern IntPtr SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        static extern UInt32 GetLastError();
+    }
+}
